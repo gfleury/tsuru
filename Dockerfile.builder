2c09cd4547c8606b823f0f0f6bcad6f4ae44fdd2
gfleury <gfleury@gmail.com>
2018-10-24 17:15:43 +0200

    Fixing typo on image name

--- Dockerfile.builder
+++ Dockerfile.builder
@@ -3,5 +3,6 @@ RUN apk add --no-cache ca-certificates
 RUN apk add --no-cache --virtual git
 RUN apk add --no-cache --virtual make 
 RUN apk add --no-cache --virtual bash 
+RUN apk add gcc musl-dev --no-cache
 
 CMD ["bash"]
--- golang-docker-builder.sh
+++ golang-docker-builder.sh
@@ -1,4 +1,4 @@
 # Run:
 # docker build -t golang-builder -f Dockerfile.builder .
 # to create golang-builder
-docker run --rm -v "$PWD":/go/src/github.com/tsuru/tsuru -w /go/src/github.com/tsuru/tsuru goland-builder:latest make binaries 
+docker run --rm -v "$PWD":/go/src/github.com/tsuru/tsuru -w /go/src/github.com/tsuru/tsuru golang-builder:latest make binaries 
